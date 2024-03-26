019cd110bd5277afc4feb7d2aa39f5bcc98711f2
mcthequeen <jeremrichard20@gmail.com>
2024-03-26 18:23:29 +0100

    v01

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #API modules
-from fastapi import FastAPI, HTTPException, Depends
-from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
-from ratelimiter import RateLimiter
+from fastapi import FastAPI
+
 
 #Modules for VectorStore and inference with llm
 from langchain_community.embeddings import HuggingFaceEmbeddings
@@ -9,8 +8,6 @@ from langchain_community.vectorstores import FAISS
 from Qa_inference import Qa_inference
 
 
-#Supabase module
-import supabase
 
 #Environ
 from dotenv import load_dotenv
@@ -19,7 +16,6 @@ import os
 load_dotenv()
 
 api_key = os.getenv("MISTRAL_API_KEY") 
-
 
 
 
@@ -33,10 +29,6 @@ supabase_anon_key = os.getenv("supabase_anon_key")
 supabase_service_role_key = os.getenv("supabase_service_role_key")
 
 
-# Define a rate limiter with a specific limit per user
-rate_limiter = RateLimiter(max_calls=10, period='minute')
-
-security = HTTPBearer()
 
 
 
@@ -45,7 +37,7 @@ print("#Loading embedding retriever")
 embedding = HuggingFaceEmbeddings(model_name="dangvantuan/sentence-camembert-large")
 
 #Loading db
-db = FAISS.load_local("faiss", embedding, allow_dangerous_deserialization=True)
+db = FAISS.load_local("faiss", embedding)
 retriever = db.as_retriever(search_type="mmr", search_kwargs={"k": 10, "lambda_mult": 0.5})
 
 #Instance for Qa_inference
@@ -55,26 +47,12 @@ qa = Qa_inference(retriever=retriever, api_key=api_key)
 
 
 @app.get("/")
-
 async def hello():
     return {"hello": "wooooooorld"}
 
-async def authenticate_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
-    try:
-        token = credentials.credentials
-        user = await supabase_client.auth.get_user(token)
 
-        if user['error']:
-            raise HTTPException(status_code=401, detail='Invalid token')
-
-        user_id = user['user']['id']
-        return user_id
-    except Exception as e:
-        raise HTTPException(status_code=401, detail=str(e))
-
-@app.post('/inference')
-@rate_limiter.rate_limited('user:<user_id>')
-async def inference(user_id: str = Depends(authenticate_token)):
+@app.get('/Inference')
+async def inference():
     user_query = "C'est quoi la maladie de crohn ?"
     qa.user_query += user_query
     documents_string, _ = qa.get_documents()
